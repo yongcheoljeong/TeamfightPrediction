33713284a1718e8d8098d6b8bdd9c656871e8a06
jyc1306 <jyc1306@gmail.com>
2021-06-03 22:33:23 +0900

    init commit rm csvfiles

---
+++ FeatureSelection.py
@@ -0,0 +1,23 @@
+selected_index = [
+    'MatchId',
+    'Map',
+    'Section',
+    'Timestamp',
+    'Team',
+    'Player',
+    'Hero'
+]
+
+selected_features = [
+    'TF_order',
+    'TF_winner',
+    'TF_RCP_sum',
+    'UltimateCharge',
+    'Cooldown1%/s',
+    'Cooldown2%/s',
+    'CooldownCrouching%/s',
+    'CooldownSecondaryFire%/s',
+    'UltimatesUsed/s',
+    'RCP',
+    'Position'
+]
---
+++ mysql_auth.py
@@ -0,0 +1,17 @@
+NYXLDB_FinalStat = {
+    'dbname':'scrim_finalstat',
+    'hostname':'221.151.212.225', 
+    'username':'imt', 
+    'pwd':'gpdlzjadh', 
+    'port':3306,
+    'charset':'utf8'
+}
+
+NYXLDB_PETH = {
+    'dbname':'scrim_peth',
+    'hostname':'221.151.212.225', 
+    'username':'imt', 
+    'pwd':'gpdlzjadh', 
+    'port':3306,
+    'charset':'utf8'
+}
